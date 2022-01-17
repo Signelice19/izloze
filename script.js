fa090f8f493c96a1950a0908a43abc8dc8848471
Signelice19 <avotins71@gmail.com>
2022-01-17 15:04:05 +0200

    otrais

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert(1)
+const vardi=['Inese Zeltiņa','Gatis Krūmiņš','Artis Dāgs','Mārtiņš Smilga','Linda Baravika'];
+const balvas=['Apple dators','tālrunis','automašīna','veļasmašīna','traktors'];
+const naudaKopa=1000000;
+let uzvaretajuSkaits=5;
+let rand= Math.random()*10;
+rand= Math.floor(rand);//noapaļo uz leju
+console.log(vardi[rand]);//izvada konsolē nejauši izvēlētus vārdus
