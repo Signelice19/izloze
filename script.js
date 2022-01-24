0ae56e2ecf87d6b0dd006e3fdb4dec71c594be79
Signelice19 <avotins71@gmail.com>
2022-01-24 15:02:17 +0200

    pieci

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const vardi=['Inese Zeltiņa','Gatis Krūmiņš','Artis Dāgs','Mārtiņš Smilg
 const balvas=['Apple dators','tālrunis','automašīna','veļasmašīna','traktors'];
 const naudaKopa=1000000;
 let uzvaretajuSkaits=5;
+let rindas= document.querySelector('.rindas');
+rindas.innerHTML=' ';
+for(let i=0;i<uzvaretajuSkaits;i++){
 let rand= Math.random()*5;
 rand= Math.floor(rand);//noapaļo uz leju
-console.log(vardi[rand]);//izvada konsolē nejauši izvēlētus vārdus
+console.log(vardi[rand]);
+}
+
